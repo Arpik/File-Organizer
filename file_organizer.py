2ff7b4ef46ad4c44e1ba765be9908d6d6d791391
Arpik <arpiksargsyan@gmail.com>
2020-09-25 17:31:57 +0400

    Added a function which collects files by putting them into appropriate folders.

--- file_organizer.py
+++ file_organizer.py
@@ -8,3 +8,15 @@ def createFolder(directory):
             os.makedirs(directory)
     except OSError:
         print ('Error: Creating directory. ' +  directory)
+
+# Collects files by putting them into appropriate folders.
+def collectFiles():
+    desktopFiles = os.listdir('../Desktop/')
+    fileExtansions = ['txt', 'docx', 'pdf', 'jpg','mp4', 'png', 'py', 'bmp']
+    for file in desktopFiles:
+        for extention in fileExtansions:
+            if file.endswith(extention):
+                # print(extention.upper())
+                createFolder('../Desktop/{}'.format(extention.upper()))
+                shutil.move('/Users/Roger/Desktop/{}'.format(file), '../Desktop/{}'.format(extention.upper()))
+collectFiles()
