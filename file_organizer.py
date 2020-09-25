e308fc8a68c428ecb83ef5d9a7eb80ea19d11340
Arpik <arpiksargsyan@gmail.com>
2020-09-25 17:27:01 +0400

    Added a function which creates a new directory.

---
+++ file_organizer.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+# Create new directory
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
