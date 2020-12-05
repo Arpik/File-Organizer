1642fccd5971d3d07111d85b9ce4b21317c6c801
Arpik <arpiksargsyan@gmail.com>
2020-12-05 17:03:49 +0400

    Received the path parameter from the command line and changed "\" to "/" to make path value valid.

--- file_organizer.py
+++ file_organizer.py
@@ -10,8 +10,10 @@ def createFolder(directory):
         print ('Error: Creating directory. ' +  directory)
 
 # Collects files by putting them into appropriate folders.
-def collectFiles():
-    desktopFiles = os.listdir('../Desktop/')
+def collectFiles(path):
+    # Receives path from the command line and changes \\ to / 
+    path = sys.argv[1].replace('\\', '/')
+    
     fileExtansions = ['txt', 'docx', 'pdf', 'jpg','mp4', 'png', 'py', 'bmp']
     for file in desktopFiles:
         for extention in fileExtansions:
@@ -19,4 +21,12 @@ def collectFiles():
                 # print(extention.upper())
                 createFolder('../Desktop/{}'.format(extention.upper()))
                 shutil.move('/Users/Roger/Desktop/{}'.format(file), '../Desktop/{}'.format(extention.upper()))
-collectFiles()
+collectFiles('../Desktop/')
+
+# def collectFiles(path):
+# # loc = r'C:\Users\Roger\Desktop'
+#     for file in os.listdir(path):
+#         fName, fExt = os.path.splitext(file) 
+#         print(fExt)
+
+# collectFilesf
